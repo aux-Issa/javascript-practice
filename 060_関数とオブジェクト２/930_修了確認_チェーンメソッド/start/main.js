800f90f049e3de6bdf78f664a54b3e8bfbf0b064
aux-Issa <issa1116184028@gmail.com>
2021-11-21 22:23:35 +0900

    finish 930

--- 060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
+++ 060_関数とオブジェクト２/930_修了確認_チェーンメソッド/start/main.js
@@ -23,15 +23,74 @@
  * 	.mutiply()
  * 	.set(6) -> '42'を出力（10 - 3) * 6
  */
+// class Calculator{
+// 	constructor(num){
+// 		this.num = null;
+// 		this._operater;
+// 	}
+// 	set(num){
+// 		if(this.num === null){
+// 			this.num = num
+// 		}else{
+// 			this._operater(this.num, num)
+// 		}
+// 		return this
+// 	}
+// 	minus(){
+// 		this._operater = function(num1, num2){
+// 			const result = num1 - num2
+// 			this._equal(result)
+// 		} 
+// 		return this
+// 	}
+// 	_equal(result){
+// 		this.num = result
+// 		console.log(result)
+// 	}
+// }
+// const calc = new Calculator();
 
-const calc = new Calculator();
+// calc.set(10)
+// 	.minus()
+// 	.set(3)
+// 	// .mutiply()
+// 	// .set(6)
+// 	// .divide()
+// 	// .set(2)
+// 	// .plus()
+// 	// .set(2)
+class Calculator {
+	constructor(){
+		this.num = null;
+		this._calculate;
+	}
+	set(num){
+		if(this.num === null){
+			this.num = num
+		}else{
+			this._calculate(this.num,  num);
+		}
+		return this;
+	}
+	multiply(){
+		this._calculate = function(initialNum, num){
+			const result = initialNum * num;
+        	this._outPutandSet(result)
+		}
+		return this;
+	}
+	divide(){
+		this._calculate = function(initialNum, num){
+			const result = initialNum / num;
+			this._outPutandSet(result)
+		}
+		return this;
+	}
+	_outPutandSet(result){
+		console.log(result)
+		this.num = result
+	}
+}
+const calc = new Calculator()
 
-calc.set(10)
-	.minus()
-	.set(3)
-	.mutiply()
-	.set(6)
-	.divide()
-	.set(2)
-	.plus()
-	.set(2)
+calc.set(10).multiply().set(100).divide().set(100000)
