a21958162ede95f968323efb263eff3e0b05fb98
aux-Issa <issa1116184028@gmail.com>
2021-11-27 16:36:49 +0900

    finish 120

--- 080_非同期処理/120_例外処理とエラー/start/main.js
+++ 080_非同期処理/120_例外処理とエラー/start/main.js
@@ -1,4 +1,26 @@
+async function fetchUsers(){
+    const rawData = await fetch('users.json')
+    if (rawData.ok) {
+        const jsonedData = await rawData.json();
+        if (!jsonedData.length) {
+            throw new Error();
+        }
+        return jsonedData
+    }
+}
 
+async const handleFetchedData = (jsonedData) => {
+    try {
+        const users = await fetchUsers();
+        for(const user of users){
+            console.log(`${user.name}`)
+        }
+    } catch(e) {
+        console.log(e)
+    } finally {
+        console.log('bye')
+    }
+}
 
 // async function fetchUsers() {
 //   const response = await fetch('users.json');
